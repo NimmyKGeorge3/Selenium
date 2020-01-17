b73f1ac93565e168e3451402240611b6b48169d8
nimmykgeorge <nimmykgeorge3@gmail.com>
2020-01-17 11:37:40 -0500

    new code

--- Implicit Wait/src/AlaskaTrip.java
+++ Implicit Wait/src/AlaskaTrip.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AlaskaTrip {
-
+// add 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:/chrome_driver/chromedriver.exe");
